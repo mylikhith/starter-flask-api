e69b56223b8d239a9218655290dd5a150414be0c
mylikhith <mylikhith@gmail.com>
2024-02-18 15:01:45 +0530

    model

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
-import os
+from flask import Flask, request, jsonify
+import pandas as pd
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
+model_filename = "model_lgbm_optimized.pkl"
+with open(model_filename, "rb") as file:
+    model = pickle.load(file)
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, world!'
+    return "Hello, world!"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Extracting form data
+        data_dict = {key: float(value) for key, value in request.form.items()}
+        query_df = pd.DataFrame([data_dict])
+
+        # Make prediction
+        prediction = model.predict(query_df)
+
+        # Ensure output is in a serializable format
+        prediction_list = prediction.tolist()
+        return jsonify({"prediction": prediction_list})
+    except Exception as e:
+        return jsonify({"error": str(e)})
